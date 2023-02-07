5aeb8b4f53ce39779c8df9cfd60423b004b22d2f
Ojaswi Saxena <ojaswisaxena14@gmail.com>
2023-02-07 10:54:54 +0530

    First commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"myapp/pkg/config"
+	"myapp/pkg/models"
+	"myapp/pkg/render"
+	"net/http"
+)
+
+// Repo is the repository used by the handlers
+var Repo *Repository
+
+// Repository is repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// send data to template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
